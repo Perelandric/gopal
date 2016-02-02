de6ad463798d9b950d7c1e7bc971c4285e603dd0
patrick dw <algorithmicimperative@gmail.com>
2016-02-02 17:27:29 -0600

    updates

--- basic_types.go
+++ basic_types.go
@@ -413,52 +413,6 @@ func (l links) get(r relTypeEnum) (string, string) {
 	Description string `json:"description"` --read
 */
 
-/*
-// TODO: This is used both for Paypal and Credit cards. In the case of Paypal,
-// many (most) of the fields may not be included in a requrest. Should we have
-// a separate `PaypalPayerInfo` object? It could be embedded into the main
-// `PayerInfo` object.
-
-// This object is pre-filled by PayPal when the payment_method is paypal.
-@struct PayerInfo
-	// Email address representing the payer. 127 characters max.
-	Email string `json:"email,omitempty"` --read --write
-
-	// Salutation of the payer.
-	Salutation string `json:"salutation,omitempty"` --read --write
-
-	// Suffix of the payer.
-	Suffix string `json:"suffix,omitempty"` --read --write
-
-	// Two-letter registered country code of the payer to identify the buyer country.
-	CountryCode CountryCodeEnum `json:"country_code,omitempty"` --read --write
-
-	// Phone number representing the payer. 20 characters max.
-	Phone string `json:"phone,omitempty"` --read --write
-
-	// Payer’s tax ID type. Allowed values: BR_CPF or BR_CNPJ. Only supported when
-	// the payment_method is set to paypal.
-	TaxIdType TaxIdTypeEnum `json:"tax_id_type,omitempty"` --read --write
-
-	// Payer’s tax ID. Only supported when the payment_method is set to paypal.
-	TaxId string `json:"tax_id,omitempty"` --read --write
-
-	// First name of the payer. Value assigned by PayPal.
-	FirstName string `json:"first_name,omitempty"` --read
-
-	// Middle name of the payer. Value assigned by PayPal.
-	MiddleName string `json:"middle_name,omitempty"` --read
-
-	// Last name of the payer. Value assigned by PayPal.
-	LastName string `json:"last_name,omitempty"` --read
-
-	// PayPal assigned Payer ID. Value assigned by PayPal.
-	PayerId string `json:"payer_id,omitempty"` --read
-
-	// Shipping address of payer PayPal account. Value assigned by PayPal.
-	ShippingAddress *ShippingAddress `json:"shipping_address,omitempty"` --read
-*/
-
 func (self *PayerInfo) validate() error {
 	// TODO: Implement
 	if self == nil {
--- credit_card_types.go
+++ credit_card_types.go
@@ -22,3 +22,43 @@ func (self *creditCardPayer) validate() error {
 	}
 	return nil
 }
+
+/*
+@struct PayerInfo
+	// Email address representing the payer. 127 characters max.
+	Email string `json:"email,omitempty"` --read --write
+
+	// Salutation of the payer.
+	Salutation string `json:"salutation,omitempty"` --read --write
+
+	// Suffix of the payer.
+	Suffix string `json:"suffix,omitempty"` --read --write
+
+	// Two-letter registered country code of the payer to identify the buyer country.
+	CountryCode CountryCodeEnum `json:"country_code,omitempty"` --read --write
+
+	// Phone number representing the payer. 20 characters max.
+	Phone string `json:"phone,omitempty"` --read --write
+
+	// Payer’s tax ID type. Allowed values: BR_CPF or BR_CNPJ. Only supported when
+	// the payment_method is set to paypal.
+	TaxIdType TaxIdTypeEnum `json:"tax_id_type,omitempty"` --read --write
+
+	// Payer’s tax ID. Only supported when the payment_method is set to paypal.
+	TaxId string `json:"tax_id,omitempty"` --read --write
+
+	// First name of the payer. Value assigned by PayPal.
+	FirstName string `json:"first_name,omitempty"` --read
+
+	// Middle name of the payer. Value assigned by PayPal.
+	MiddleName string `json:"middle_name,omitempty"` --read
+
+	// Last name of the payer. Value assigned by PayPal.
+	LastName string `json:"last_name,omitempty"` --read
+
+	// PayPal assigned Payer ID. Value assigned by PayPal.
+	PayerId string `json:"payer_id,omitempty"` --read
+
+	// Shipping address of payer PayPal account. Value assigned by PayPal.
+	ShippingAddress *ShippingAddress `json:"shipping_address,omitempty"` --read
+*/
